f5ba45baa85bd5163e4e0762c7787ebf6938670f
Luiz Gustavo <luiz.ro.aguiar@gmail.com>
2018-04-03 00:58:47 -0300

    Otimizacao do algoritmo

--- src/main/java/com/s2it_2/Matrix.java
+++ src/main/java/com/s2it_2/Matrix.java
@@ -7,31 +7,19 @@ package com.s2it_2;
 public class Matrix {
 
 	public int diffDiagonal(int[][] mtrx, int n) {
-		return somaDiagPrincipal(mtrx) - somaDiagSecundaria(mtrx, n);
-	}
-
-	private int somaDiagPrincipal(int mtrx[][]) {
-		int somaDiagonal = 0;
+		int somaDiagonalPrincipal = 0;
+		int somaDiagonalSecundaria = 0;
 		for (int i = 0; i < mtrx.length; i++) {
 			for (int j = 0; j < mtrx.length; j++) {
 				if (i == j) {
-					somaDiagonal += mtrx[i][j];
+					somaDiagonalPrincipal += mtrx[i][j];
 				}
-			}
-		}
-		return somaDiagonal;
-	}
-
-	private int somaDiagSecundaria(int mtrx[][], int n) {
-		int somaDiagonal = 0;
-		for (int i = 0; i < mtrx.length; i++) {
-			for (int j = 0; j < mtrx.length; j++) {
 				if (n - 1 - j == i) {
-					somaDiagonal += mtrx[i][j];
+					somaDiagonalSecundaria += mtrx[i][j];
 				}
 			}
 		}
-		return somaDiagonal;
+		return somaDiagonalPrincipal - somaDiagonalSecundaria;
 	}
 
 }
